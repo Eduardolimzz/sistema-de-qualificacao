915cad6453a1fa820c4f5ca7c643af71c12a8b03
Eduardolimzz <eduardoaluno1800@gmail.com>
2025-10-21 19:07:20 -0300

    feat(setup): Resolve #1: Configuração do esqueleto Spring e .gitignore.

---
+++ src/main/java/com/sistema_de_qualificacao/sistema_de_qualificacao/SistemaDeQualificacaoApplication.java
@@ -0,0 +1,13 @@
+package com.sistema_de_qualificacao.sistema_de_qualificacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeQualificacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeQualificacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sistema_de_qualificacao/sistema_de_qualificacao/SistemaDeQualificacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema_de_qualificacao.sistema_de_qualificacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeQualificacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
