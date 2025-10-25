40ece515789700705f9616bc58d075d5a5b3c91c
Eduardolimzz <eduardoaluno1800@gmail.com>
2025-10-25 11:59:18 -0300

    feat: finaliza tabela aluno com CRUD implementado. Closes #6

---
+++ src/main/java/com/sistema_de_qualificacao/sistema_de_qualificacao/dto/UpdateAlunoDto.java
@@ -0,0 +1,4 @@
+package com.sistema_de_qualificacao.sistema_de_qualificacao.dto;
+
+public record UpdateAlunoDto(String nomealuno, String senhaaluno) {
+}
--- src/main/java/com/sistema_de_qualificacao/sistema_de_qualificacao/service/AlunoService.java
+++ src/main/java/com/sistema_de_qualificacao/sistema_de_qualificacao/service/AlunoService.java
@@ -1,10 +1,13 @@
 package com.sistema_de_qualificacao.sistema_de_qualificacao.service;
 
 import com.sistema_de_qualificacao.sistema_de_qualificacao.dto.CreateAlunoDto;
+import com.sistema_de_qualificacao.sistema_de_qualificacao.dto.UpdateAlunoDto;
 import com.sistema_de_qualificacao.sistema_de_qualificacao.entity.Aluno;
 import com.sistema_de_qualificacao.sistema_de_qualificacao.repository.AlunoRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -25,4 +28,48 @@ public class AlunoService {
         var alunoSaved = alunoRepository.save(entity);
         return alunoSaved.getAlunoId();
     }
+
+    //metodo publico que vai retornar o próprio aluno
+
+    public Optional<Aluno> getAlunoById(String alunoId){
+        //recebemos uma string e convertemos para UUID
+        return alunoRepository.findById(UUID.fromString(alunoId));
+    }
+
+    public List<Aluno> listAlunos(){
+        return alunoRepository.findAll();
+    }
+
+    public void updateAlunoById(String alunoId,
+                                UpdateAlunoDto updateAlunoDto){
+
+        var id = UUID.fromString(alunoId);
+
+        var alunoEntity = alunoRepository.findById(id);
+
+        if(alunoEntity.isPresent()){
+            var aluno = alunoEntity.get();
+
+            if(updateAlunoDto.nomealuno() != null){
+                aluno.setNomealuno(updateAlunoDto.nomealuno());
+            }
+            if(updateAlunoDto.senhaaluno() != null){
+                aluno.setSenhaaluno(updateAlunoDto.senhaaluno());
+            }
+            alunoRepository.save(aluno);
+        }
+    }
+
+    public void deleteById(String alunoId){
+
+        var id = UUID.fromString(alunoId);
+
+        var alunoExists = alunoRepository.existsById(id);
+
+        if(alunoExists){
+            alunoRepository.deleteById(id);
+        }
+    }
+
+
 }
