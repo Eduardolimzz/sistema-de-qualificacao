7a7e7081390c1ce244a7f073a6e4e92879546854
ianmelo1 <ianmelo171@gmail.com>
2025-10-27 21:28:03 -0300

    fix: Organizando Pastas, e criando componentes essências como o README do Frontend - fix #4

--- src/main/resources/frontend/src/App.tsx
+++ src/main/resources/frontend/src/App.tsx
@@ -1,13 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import Layout from './componentes/Layout/';
-import AlunosPage from "./Pages/AlunosPage/AlunosPage";
+import PaginaHome from './Pages/Public/PaginaHome'
+import AlunosPage from "./Pages/CRUD/AlunosPage/AlunosPage";
 
 
 function App() {
   return (
   <Routes>
           <Route path="/" element={<Layout />}>
-            <Route index element={<AlunosPage />} />
+            <Route index element={<PaginaHome />} />
             <Route path="*" element={<h2>Página não encontrada dentro do Layout</h2>} />
           </Route>
         </Routes>
