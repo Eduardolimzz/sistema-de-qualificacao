fbb6a3cf8df99be2d5750a5e5a449a481945b58c
Eduardolimzz <eduardoaluno1800@gmail.com>
2025-10-30 18:57:49 -0300

    feat: implementa service e DTOS da tabela curso

---
+++ src/main/java/com/sistema_de_qualificacao/sistema_de_qualificacao/dto/CreateCursoDto.java
@@ -0,0 +1,14 @@
+package com.sistema_de_qualificacao.sistema_de_qualificacao.dto;
+
+public class CreateCursoDto {
+
+    private String duracao_curso;
+    private String nomecurso;
+    private String nivel_curso;
+
+    public CreateCursoDto(String duracao_curso, String nomecurso, String nivel_curso) {
+        this.duracao_curso = duracao_curso;
+        this.nomecurso = nomecurso;
+        this.nivel_curso = nivel_curso;
+    }
+}
--- src/main/java/com/sistema_de_qualificacao/sistema_de_qualificacao/service/CursoService.java
+++ src/main/java/com/sistema_de_qualificacao/sistema_de_qualificacao/service/CursoService.java
@@ -1,7 +1,73 @@
 package com.sistema_de_qualificacao.sistema_de_qualificacao.service;
 
+import com.sistema_de_qualificacao.sistema_de_qualificacao.dto.CreateCursoDto;
+import com.sistema_de_qualificacao.sistema_de_qualificacao.dto.UpdateCursoDto;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class CursoService {
+
+    private final CursoRepository cursoRepository;
+
+    public CursoService(CursoRepository cursoRepository) {
+        this.cursoRepository = cursoRepository;
+    }
+
+    public UUID createCurso(CreateCursoDto createCursoDto){
+        var entity = new Curso();
+        entity.setDuracao_cursocurso(createCursoDto.getCursoduracao_curso());
+        entity.setNomecursocurso(createCursoDto.getNomecursocurso());
+        entity.setNivel_cursocurso(createCursoDto.getNivel_cursocurso());
+
+        var cursoSaved = cursoRepository.save(entity);
+        return cursoSaved.getCursoId();
+    }
+
+    public Optional<Curso> getCursoById(String cursoId){
+        return cursoRepository.findById(UUID.fromString(cursoId));
+    }
+
+    public List<Curso> listCursos(){
+        return cursoResitory.findAll();
+    }
+
+    public void updateCursoById(String cursoId,
+                                UpdateCursoDto updateCursoDto){
+
+        var id = UUID.fromString(cursoId);
+
+        var cursoEntity = cursoRepository.findyById(id);
+
+        if(cursoEntity.isPresent()){
+            var curso = cursoEntity.get();
+
+            if(updateCursoDto.nomecurso() != null){
+                curso.setDuracao_cursocurso(updateCursoDto.duracaocurso());
+            }
+
+            if(updateCursoDto.nomecurso() != null){
+                curso.setNomecursocurso(updateCursoDto.nomecurso());
+            }
+
+            if(updateCursoDto.nomecurso() != null){
+                curso.setNivel_cursocurso(updateCursoDto.nivel_curso());
+            }
+
+            cursoRepository.save(curso);
+        }
+    }
+
+    public void deleteById(String cursoId){
+
+        var id = UUID.fromString(cursoId);
+
+        var alunoExists = cursoRepository.existsById(id);
+
+        if(cursoExists){
+            cursoRepository.deleteById(id);
+        }
+    }
+
 }
